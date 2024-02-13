7bf456fcc7922f53e5a595d2fb827e09b2564352
MarvJungs <Marvin10072000@live.de>
2024-02-13 02:27:17 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using Octokit;
+using System.Diagnostics;
+using System.Net;
+using System.IO.Compression;
+
+UpdateManager um = new UpdateManager("v3b");
+um.UpdateEmulator();
+/*GitHubClient client = new GitHubClient(new ProductHeaderValue("app"));
+
+Task<IReadOnlyList<Release>> versionTask = client.Repository.Release.GetAll("LunaticShiN3", "Luna-Project64");
+Task<IReadOnlyList<ReleaseAsset>> assetTask = client.Repository.Release.GetAllAssets("LunaticShiN3", "Luna-Project64", versionTask.Result[0].Id);
+
+try
+{
+	//Step 2: Kill Project64 Processes if such exist
+	if(Process.GetProcessesByName("Project64").Length > 0)
+	{
+		foreach (Process emulator  in Process.GetProcessesByName("Project64"))
+		{
+			emulator.Kill();
+			Console.WriteLine("Killed Project64");
+		}
+	}
+
+	//Step 5: Delete Project64.exe
+	string extractPath = Directory.GetCurrentDirectory();
+	File.Delete(Path.Combine(extractPath, "Project64.exe"));
+
+	//Step 3: Download the most recent Version from Github
+	using (var webClient =  new WebClient())
+	{
+		webClient.DownloadFile(assetTask.Result[0].BrowserDownloadUrl, assetTask.Result[0].Name);
+		Console.WriteLine("Done Downloading!");
+	}
+
+	//Step 4: Ensure that the path name is properly formatted
+	if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+		extractPath += Path.DirectorySeparatorChar;
+
+
+
+	//Step 6: Extract the zip-File
+	using (ZipArchive archive = ZipFile.OpenRead(assetTask.Result[0].Name))
+	{
+		foreach(ZipArchiveEntry entry in archive.Entries) 
+		{
+			if(entry.Name == "Project64.exe")
+			{
+				string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.Name));
+                Console.WriteLine(entry.Name);
+
+                if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal)) Console.WriteLine();
+                entry.ExtractToFile(destinationPath);
+            }
+		}
+	}
+	Console.WriteLine("Done Extracting!");
+
+	//Step 7: Delete the downloaded zip-File
+	File.Delete(assetTask.Result[0].Name);
+	Console.WriteLine("Done Deleting!");
+
+	//Step 8: Start Project64 again
+	Process.Start(extractPath + "Project64.exe");
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+*/
+
+
+Environment.Exit(0);
---
+++ ConsoleApp1/UpdateManager.cs
@@ -0,0 +1,110 @@
+﻿using Octokit;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Xml.Linq;
+
+public class UpdateManager
+{
+	GitHubClient client;
+	Task<IReadOnlyList<Release>> _releases;
+	Task<IReadOnlyList<ReleaseAsset>> _releaseAssets;
+	string extractPath;
+	string versionString;
+
+	readonly string EMULATOR = "Project64";
+	readonly string REPO_OWNER = "LunaticShiN3";
+	readonly string REPO_NAME = "Luna-Project64";
+
+	readonly string[] REPLACEABLE_FILE_NAMES = ["dgVoodoo for Jabo.zip", "parasettings.ini", "Project64.exe"];
+	readonly string[] REPLACEABLE_FOLDER_NAMES = ["Lang", "Logs", "MemPaks", "Plugin"];
+
+	public UpdateManager(string versionString)
+	{
+		this.client = new GitHubClient(new ProductHeaderValue("PJ64-Auto-Updater"));
+		this._releases = client.Repository.Release.GetAll(REPO_OWNER, REPO_NAME);
+		this._releaseAssets = client.Repository.Release.GetAllAssets(REPO_OWNER, REPO_NAME, _releases.Result[0].Id);
+		this.extractPath = Directory.GetCurrentDirectory();
+		this.versionString = versionString;
+	}
+
+	private GitHubClient GetGitHubClient() { return client; }
+	private Task<IReadOnlyList<Release>> GetReleases() { return _releases; }
+	private Task<IReadOnlyList<ReleaseAsset>> GetReleaseAssets() { return _releaseAssets; }
+	private string getExtractPath() { return extractPath; }
+
+	public void UpdateEmulator()
+	{
+		extractPath = NormalizePathName(extractPath);
+
+		KillEmulatorProcess();
+		DeleteEmulator();
+		DownloadMostRecentVersion();
+		ExtractZipFile();
+		DeleteZipFile();
+		StartUpEmulator();
+	}
+
+	private void KillEmulatorProcess()
+	{
+		if (Process.GetProcessesByName(EMULATOR).Length > 0)
+		{
+			foreach (Process emulator in Process.GetProcessesByName(EMULATOR))
+			{
+				emulator.Kill();
+				Console.WriteLine($"Killed {EMULATOR}");
+			}
+		}
+	}
+
+	private void DeleteEmulator()
+	{
+		foreach (var fileName in REPLACEABLE_FILE_NAMES)
+		{
+			if(File.Exists(fileName))
+				File.Delete(Path.Combine(getExtractPath(), fileName));
+		}
+		foreach (var dirName in REPLACEABLE_FOLDER_NAMES)
+		{
+			if(Directory.Exists(dirName))
+				Directory.Delete(Path.Combine(getExtractPath(), dirName), true);
+		}
+	}
+
+	private void DownloadMostRecentVersion()
+	{
+		using (var webClient = new WebClient())
+		{
+			webClient.DownloadFile(GetReleaseAssets().Result[0].BrowserDownloadUrl, GetReleaseAssets().Result[0].Name);
+		}
+	}
+
+	private string NormalizePathName(string extractPath)
+	{
+		if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+			extractPath += Path.DirectorySeparatorChar;
+		return extractPath;
+	}
+
+	private void ExtractZipFile()
+	{
+		
+		string destinationPath = getExtractPath();
+		ZipFile.ExtractToDirectory(GetReleaseAssets().Result[0].Name, destinationPath, true);
+			
+		
+	}
+
+	private void DeleteZipFile()
+	{
+		File.Delete(GetReleaseAssets().Result[0].Name);
+	}
+
+	private void StartUpEmulator()
+	{
+		Process.Start(getExtractPath() + $"{EMULATOR}.exe");
+	}
+
+
+}
